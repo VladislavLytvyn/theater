dfe541ad13196628d294009bafbea6fe226c521e
v_lytvyn <apoints96@gmail.com>
2024-03-17 20:14:27 +0200

    fix timeout

--- theater.py
+++ theater.py
@@ -395,4 +395,4 @@ while True:
         msg = prepare_mail("Cycle is still working.", "Cycle is still working.", revert_sender, revert_recipients)
         send_mail(msg)
     print(f"{datetime.now()}: end cycle.")
-    time.sleep(60)
+    time.sleep(30)
